e3f334deb42b55a43fea9b46c92d2c258d78bbcf
Dana Sugu <dana.sugu@gmail.com>
2021-08-26 14:45:25 +0300

    cont.

--- wp-content/themes/photo-snow/header.php
+++ wp-content/themes/photo-snow/header.php
@@ -18,6 +18,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,400;0,800;1,100;1,400&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
